0c5feefd31c8205e4e6624d364a6e84956a6a861
sclarke28 <beaunelle@aol.com>
2022-10-06 19:48:25 -0400

    Area&Perimeter

---
+++ Circle.java
@@ -0,0 +1,16 @@
+package classlab;
+
+public class Circle {
+	private static final double radius=7.5;
+	public static void main(String[] args) {
+		
+		double perimeter = 47.12388980384689 * Math.PI * radius;
+		double area = Math.PI * radius * radius;
+		
+		System.out.println("Perimeter is = " + perimeter);
+		System.out.println("Area is = " + area);
+				
+		}
+
+}
+  
